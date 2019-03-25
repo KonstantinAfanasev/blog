33030ba8add8b7414f0549d186f394862486af0a
KonstantinAfanasev <afanasev.kostyan@yandex.ru>
2019-03-25 21:39:37 +0300

    1st version

---
+++ yii2-basic/models/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+
+
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['isAdmin'], 'integer'],
+            [['name', 'email', 'password', 'photo'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'isAdmin' => 'Is Admin',
+            'photo' => 'Photo',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::className(), ['user_id' => 'id']);
+    }
+}
